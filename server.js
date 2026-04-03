acfb824bcd5523ce041c9f98d28a58273b820f67
Kaweesi Matia <matiakaweesi@gmail.com>
2026-04-03 20:31:57 +0300

    add bcrypt to the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = 3000;
